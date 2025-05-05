d6532926a96e2b453ed0932ad30dc6a4e557ff10
Carla Rendón <csofia1429@gmail.com>
2025-05-04 22:32:58 -0500

    First file upload test

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Dobby-File-Manager
+
 Dobby is a free elf (but cloud storage isn’t)!" – Works tirelessly in the background to store (upload - download), organize, and protect files (GCP - S3), with metadata and digital signatures—no socks required.
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,25 @@
+import { Storage } from '@google-cloud/storage';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private readonly storage: Storage;
+  constructor() {
+    const GCP_PROJECT_ID = 'winter-form-458923-n4';
+    const GCP_KEY_FILE_PATH = 'key-gcp.json';
+
+    this.storage = new Storage({
+      projectId: GCP_PROJECT_ID,
+      keyFilename: GCP_KEY_FILE_PATH,
+    });
+  }
+
+  async uploadFileGcp() {
+    const GCP_BUCKET = 'dobby-file-manager';
+    const bucket = this.storage.bucket(GCP_BUCKET);
+    const result = await bucket.upload(
+      'C:\\Users\\csofi\\Documents\\Dobby-File-Manager\\files\\slide1.png',
+    );
+    return result;
   }
 }
