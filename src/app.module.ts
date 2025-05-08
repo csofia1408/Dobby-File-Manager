75bba0ff5efb69c564de3fdde3b4ca3e9ac2f2bc
Tomás Bernal <tbernalz@eafit.edu.co>
2025-05-08 09:52:22 -0500

    feat(rabbitmq) add rabbitmq config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { GcpStorageService } from './gcp-storage.service';
 import { GcpUploadController } from './gcp-upload.controller';
+import { UserModule } from './user/user.module';
 
 @Module({
   controllers: [GcpUploadController],
   providers: [GcpStorageService],
+  imports: [UserModule],
 })
 export class AppModule {}
---
+++ src/rabbitmq/rabbitmq.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { RabbitMQModule } from '@golevelup/nestjs-rabbitmq';
+import { RABBITMQ_CONFIG } from 'src/config/rabbitmq.constants';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+@Module({
+  imports: [
+    RabbitMQModule.forRoot({
+      exchanges: [],
+      uri: `amqp://${process.env.RABBITMQ_USER}:${process.env.RABBITMQ_PASS}@${process.env.RABBITMQ_HOST}:${process.env.RABBITMQ_PORT}/${process.env.RABBITMQ_VHOST}`,
+    }),
+  ],
+  exports: [RabbitMQModule],
+})
+export class RabbitMQSharedModule {}
